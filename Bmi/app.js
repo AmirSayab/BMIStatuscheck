3e1d4125a570c3ae0ca40e825d47cc60a1637dcb
AmirSayab <111190828+AmirSayab@users.noreply.github.com>
2022-10-22 11:36:06 +0500

    Add files via upload
    Write a script that calculates the user’s body mass index (BMI) and categorizes it as underweight,
    normal,overweight, or obese, based on the table from the United States Centers for Disease Control:

---
+++ Bmi/app.js
@@ -0,0 +1,16 @@
+function message() {
+    var bmi = prompt("Enter your BMI To see status: ")
+    if (bmi < 18.5) {
+        alert("Underweight");
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        alert("Normal");
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        alert("Overweight");
+    } else if (bmi >= 30) {
+        alert("Obese");
+    } {
+
+    } {
+
+    }
+}
